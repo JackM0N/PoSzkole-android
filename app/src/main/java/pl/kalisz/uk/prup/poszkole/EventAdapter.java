cd7f2efb26f449613927c542a541fb8b385755c9
JackM0N <przemyslawgotowala@gmail.com>
2024-05-28 13:06:50 +0200

    Added highlighting days with event in them

---
+++ app/src/main/java/pl/kalisz/uk/prup/poszkole/EventAdapter.java
@@ -0,0 +1,81 @@
+package pl.kalisz.uk.prup.poszkole;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class EventAdapter extends ArrayAdapter<Event> {
+
+    private Date selectedDate;
+    private String eventId;
+    private List<String> eventIdList;
+    public EventAdapter(@NonNull Context context, @NonNull List<Event> objects, List<String> eventIdList) {
+        super(context, 0, objects);
+        this.eventIdList = eventIdList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_event, parent, false);
+        }
+
+        Event event = getItem(position);
+
+        TextView textViewImie = convertView.findViewById(R.id.textViewImie);
+        TextView textViewPrzedmiot = convertView.findViewById(R.id.textViewPrzedmiot);
+        TextView textViewGodzinaOd = convertView.findViewById(R.id.textViewGodzinaOd);
+        TextView textViewGodzinaDo = convertView.findViewById(R.id.textViewGodzinaDo);
+
+        if (event != null) {
+            textViewImie.setText(event.getName());
+            textViewPrzedmiot.setText(event.getSubject());
+            textViewGodzinaOd.setText(event.getFrom());
+            textViewGodzinaDo.setText(event.getTo());
+            Log.d("EventAdapter", "Event: " + event.getName() + ", " + event.getSubject() + ", " + event.getFrom() + ", " + event.getTo());
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getContext(), EditEventActivity.class);
+                    intent.putExtra("eventId", eventIdList.get(position));
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    String dateString = dateFormat.format(selectedDate);
+                    intent.putExtra("selectedDate", dateString);
+                    getContext().startActivity(intent);
+                }
+            });
+        }else {
+            Log.d("EventAdapter", "Event jest null");
+        }
+
+        return convertView;
+    }
+
+    public void setSelectedDate(Date date) {
+        this.selectedDate = date;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+}
+
+
---
+++ app/src/main/java/pl/kalisz/uk/prup/poszkole/MainActivity.java
@@ -0,0 +1,168 @@
+package pl.kalisz.uk.prup.poszkole;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.applandeo.materialcalendarview.CalendarDay;
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.listeners.OnCalendarDayClickListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private ListView listView;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Calendar");
+    private Date selectedDate;
+    private EventAdapter eventAdapter;
+    private List<Event> eventList;
+    private List<String> eventIdList;
+    private List<String> storedDates;
+    private List<CalendarDay> calendarDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+        listView = findViewById(R.id.listView);
+        selectedDate = Calendar.getInstance().getTime();
+
+        eventList = new ArrayList<>();
+        eventIdList = new ArrayList<>();
+        storedDates = new ArrayList<>();
+        calendarDays = new ArrayList<>();
+        eventAdapter = new EventAdapter(this, eventList, eventIdList);
+        listView.setAdapter(eventAdapter);
+
+        eventAdapter.setSelectedDate(selectedDate);
+
+        fetchEventsForSelectedDate();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                storedDates.clear();
+                for (DataSnapshot dateSnapshot : dataSnapshot.getChildren()) {
+                    String date = dateSnapshot.getKey();
+                    if (date != null) {
+                        storedDates.add(date);
+                        CalendarDay calendarDay = convertToCalendarDay(date);
+                        if (calendarDay != null) {
+                            calendarDays.add(calendarDay);
+                        }
+                    }
+                }
+
+                calendarView.setCalendarDays(calendarDays);
+
+                Log.d("CalendarDays", calendarDays.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting data failed, log a message
+                Log.w("MainActivity", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+
+        calendarView.setOnCalendarDayClickListener(new OnCalendarDayClickListener() {
+            @Override
+            public void onClick(@NonNull CalendarDay calendarDay) {
+                selectedDate = calendarDay.getCalendar().getTime();
+                eventAdapter.setSelectedDate(selectedDate);
+                eventAdapter.setEventId(databaseReference.child(selectedDate.toString()).getKey());
+                fetchEventsForSelectedDate();
+            }
+        });
+
+        FloatingActionButton button = findViewById(R.id.add);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedDate == null) {
+                    selectedDate = Calendar.getInstance().getTime();
+                }
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String dateString = dateFormat.format(selectedDate);
+
+                Intent intent = new Intent(MainActivity.this, AddEvent.class);
+                intent.putExtra("selectedDate", dateString);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void fetchEventsForSelectedDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = dateFormat.format(selectedDate);
+
+        databaseReference.child(dateString).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                eventList.clear();
+                eventIdList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Event event = snapshot.getValue(Event.class);
+                    if (event != null) {
+                        Log.d("MainActivity", "Event: " + event.getName() + ", " + event.getSubject() + ", " + event.getFrom() + ", " + event.getTo());
+                        eventList.add(event);
+                        eventIdList.add(snapshot.getKey());
+                    } else {
+                        Log.d("MainActivity", "Event jest null");
+                    }
+                }
+                eventAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Coś poszło nie tak, przepraszamy", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private CalendarDay convertToCalendarDay(String date) {
+        try {
+            String[] parts = date.split("-");
+            int year = Integer.parseInt(parts[0]);
+            int month = Integer.parseInt(parts[1]);
+            int day = Integer.parseInt(parts[2]);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month - 1, day);
+
+            CalendarDay calendarDay = new CalendarDay(calendar);
+            calendarDay.setBackgroundDrawable(AppCompatResources.getDrawable(this, R.drawable.eventday_background));
+
+            return calendarDay;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
